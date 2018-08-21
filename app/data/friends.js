76b52fe334e72caf352cb6a551238b4c6349627e
gholmesjr84 <gregoryholmesjr@phantomskullgraphics.com>
2018-08-21 14:58:18 -0700

    Uploading files

---
+++ app/data/friends.js
@@ -0,0 +1,52 @@
+var friends = [
+    {
+        name:"Arthas",
+        photo: "https://s.newtalk.tw/album/news/116/5a8e8f02c4695.jpg",
+        scores:[
+        "2",
+        "1",
+        "6",
+        "2",
+        "4",
+        "3",
+        "1",
+        "2",
+        "1",
+        "3"
+        ]
+    },
+    {
+        name:"The Joker",
+        photo: "http://www.imagesyoulike.com/images/d/32x24/d3315.jpg",
+        scores:[
+        "1",
+        "1",
+        "1",
+        "1",
+        "5",
+        "1",
+        "5",
+        "3",
+        "1",
+        "4"
+        ]
+    },
+    {
+        name:"Jack Skellington",
+        photo: "https://statici.behindthevoiceactors.com/behindthevoiceactors/_img/chars/jack-skellington-kingdom-hearts-rechain-of-memories-5.75.jpg",
+        scores:[
+        "2",
+        "5",
+        "3",
+        "2",
+        "1",
+        "4",
+        "6",
+        "2",
+        "4",
+        "1"
+        ]
+    }
+];
+
+module.exports = friends;
